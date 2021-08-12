fdbad418d28fcc94fa79ca39e50878938f1aff46
zhengxiangguo <zhengxiangguo@xiaomi.com>
2021-08-12 20:50:22 +0800

    cmake-server

---
+++ 03moduo/01cmake_moduo/build/CMakeFiles/cmake_moduo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_moduo.dir/cmake_moduo.cpp.o"
+  "../bin/cmake_moduo.pdb"
+  "../bin/cmake_moduo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_moduo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 03moduo/01cmake_moduo/编译时的命令解析.md
@@ -0,0 +1,16 @@
+
+
+g++ -o main -g main.cpp xxx.cpp -I/usr/include -L/usr/lib -Imuduo_net -Imuduo_base -Ipthread
+
+main
+编译出可执行文件
+此外可以生成
+*.a   静态库
+*.so  动态库
+
+(1) 生成文件的格式
+(2) 编译时的参数
+(3) 源文件
+(4) 头文件的搜索路径
+(5) 库文件的搜索路径
+(6) 所需要链接的库
